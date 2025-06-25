f874997e894449bc9fb1d3bbeb5f81ddf4e5960d
Blake Pugh <blakepugh5@gmail.com>
2025-06-25 00:10:56 -0700

    Update shift calculator and clock out calculator cards in the main app view

--- src/components/TimecardContainer.jsx
+++ src/components/TimecardContainer.jsx
@@ -6,6 +6,10 @@ const TimeCardContainer = () => {
         <div className={styles.timecardContainer}>
             <Card className={styles.card}>
                 <h2 className={styles.cardText}>Time Card Tool</h2>
+                <div className={styles.featureDiv}>
+                    <Card className={styles.shiftCalculatorCard}></Card>
+                    <Card className={styles.clockOutCalculatorCard}></Card>
+                </div>
             </Card>
         </div>
     )
